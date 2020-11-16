c79389ca93c8cbd0160193ee9d04484249fcbfc7
eeeb0053 <eeeb0053@gmail.com>
2020-11-16 17:58:13 +0900

    17:58

---
+++ day01/Main.java
@@ -0,0 +1,10 @@
+package day01;
+
+import day01.Naming;
+import day01.Operator;
+
+public class Main {
+	public static void main(String[] args) {
+		Naming naming = new Naming();
+	}
+}
---
+++ day01/Operator.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class Operator {
+	int myNumber1;
+	int myNumber2;
+	
+	int getMyNumber1() {
+		return myNumber1;
+	}
+	void setMyNumber1(int myNumber1) {
+		this.myNumber1 = myNumber1;
+	}
+}
